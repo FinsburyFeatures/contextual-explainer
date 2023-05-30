c9ac6d7373789aa7afb9395aa0d5f8a74ee2a06e
Matt Megarry <mattmegarry@gmail.com>
2023-05-30 11:41:35 +0100

    hello world

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("I am a python")
+import os
+
+import anthropic
+from dotenv import load_dotenv
+
+load_dotenv()
+
+anthropic_client = anthropic.Client(api_key=os.getenv("ANTHROPIC_API_KEY"))
+
+print(anthropic_client)
+print("hello world")
